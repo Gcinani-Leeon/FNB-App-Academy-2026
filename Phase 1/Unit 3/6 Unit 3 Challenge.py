5a0484408b793edda9588b20c822cdda63fba572
Gcinani Leeon Xulu <gcinani.leeon011@gmail.com>
2026-07-19 11:42:38 +0200

    Update 6 Unit 3 Challenge.py

---
+++ Phase 1/Unit 3/6 Unit 3 Challenge.py
@@ -0,0 +1,6 @@
+# The South African Fuel Cost Calculator
+
+litres = float(input("How many litres of fuel do you want to purchase? "))
+price_per_litre = float(input("What is the price per litre of fuel? "))
+
+# INCOMPLETE
